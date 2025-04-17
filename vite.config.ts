e355e93ce2de4eb1ab54b21a6368a79cf3f6d89e
Bakpo Godwin <67370368+Hood-Codivo@users.noreply.github.com>
2025-04-17 13:20:35 +0100

    url added to production

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     proxy: {
       // Proxy WebSocket (and HTTP) for Socket.IO
       "/socket.io": {
-        target: "http://127.0.0.1:3000",
+        target: "https://stream-test-backend.onrender.com",
         ws: true, // enable WebSocket proxying :contentReference[oaicite:1]{index=1}
         changeOrigin: true,
         secure: false,
