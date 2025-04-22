a9eba216c1a1283353fd43baee9a390778183e29
Bakpo Godwin <67370368+Hood-Codivo@users.noreply.github.com>
2025-04-22 16:04:01 +0100

    added provider in the root file

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "sonner";
+import { WalletConnectProvider } from "./components/WalletProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+     <WalletConnectProvider>
     <BrowserRouter>
       <Toaster position="top-right" /> {/* you can change position */}
       <App />
     </BrowserRouter>
+    </WalletConnectProvider>
   </StrictMode>
 );
